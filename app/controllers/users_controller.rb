ce3e35ecfdbf28e5e2b08d8224af09ec291386b8
Robert Rosenberg <robertdr85@gmail.com>
2018-02-20 18:56:41 -0800

    Added user authorization

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,2 +1,18 @@
 class UsersController < ApplicationController
+  def create
+    user = User.new(
+      email: params[:email],
+      user_name: params[:user_name],
+      password: params[:password],
+      first_name: params[:first_name],
+      last_name: params[:last_name],
+      bio: params[:bio],
+      profile_picture: params[:profile_picture]
+    )
+    if user.save
+      render json: {message: "User created succesfully"}, status: :created
+    else
+      render json: {errors: user.errors.full_messages}, status: :bad_request
+    end
+  end
 end
