69cb1f687974a300b793d599bf0d000262de8be6
Robert Rosenberg <robertdr85@gmail.com>
2018-02-25 14:13:50 -0800

    Added seed_dump and created index html and JS

--- config/routes.rb
+++ config/routes.rb
@@ -3,15 +3,15 @@ Rails.application.routes.draw do
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 
   #User routes show, create, update, delete
-  POST 'user_token' => 'user_token#create'
-  GET "/users/:id" => "users#show"
-  POST "/users" => "users#create"
-  PUT "/users/:id" => "users#update"
-  DELETE "/users/:id" => "users#destroy"
+  post 'user_token' => 'user_token#create'
+  get "/users/:id" => "users#show"
+  post "/users" => "users#create"
+  put "/users/:id" => "users#update"
+  delete "/users/:id" => "users#destroy"
 
-  #Commitment routes index, create, update, delete
-  GET "/commitments" => "commitments#index"
-  POST "/commitments" => "commitments#create"
-  PUT "/commitments/:id" => "commitments#update"
-  DELETE "/commitments/:id" => "commitments#destroy"
+  #Commitment routes index, create, update, destroy
+  get "/commitments" => "commitments#index"
+  post "/commitments" => "commitments#create"
+  put "/commitments/:id" => "commitments#update"
+  delete "/commitments/:id" => "commitments#destroy"
 end
--- db/seeds.rb
+++ db/seeds.rb
@@ -1,48 +1,11 @@
-# This file should contain all the record creation needed to seed the database with its default values.
-# The data can then be loaded with the rails db:seed command (or created alongside the database with db:setup).
-#
-# Examples:
-#
-#   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
-#   Character.create(name: 'Luke', movie: movies.first)
-User.create(
-  {
-    email: "robertdr85@gmail.com", 
-    user_name: "robdrosen", 
-    first_name: "Robert", 
-    last_name: "Rosenberg", 
-    password: "nil", 
-    bio: "The creator of the commitment ledger application!", 
-    profile_picture: "imgur"
-  }
-)
-
-Category.create([
-  {
-    title: "Personal"
-  },
-  {
-    title: "Work"
-  }
+Category.create!([
+  {title: "Personal"},
+  {title: "Work"}
 ])
-
-Commitment.create([
-  {
-    what: "Finish my capstone project",
-    who: "myself",
-    when: "2018-03-29 21:30:00",
-    status: "Committed",
-    notes: "I'm commiting myself to making this commitment app so that I fullfill my commitment on helping other commit to their commitments :)",
-    user_id: 1,
-    category_id: 2
-  },
-  {
-    what: "Call my parents once a week",
-    who: "Mom and Dad",
-    when: "",
-    status: "Committed",
-    notes: "Keep in better touch with my parents, to make them happy!",
-    user_id: 1,
-    category_id: 2
-  }
+Commitment.create!([
+  {what: "Finish my capstone project", who: "myself", when: "2018-03-29 21:30:00", status: "Committed", notes: "I'm commiting myself to making this commitment app so that I fullfill my commitment on helping other commit to their commitments :)", user_id: 1, category_id: 2},
+  {what: "Call my parents once a week", who: "Mom and Dad", when: nil, status: "Committed", notes: "Keep in better touch with my parents, to make them happy!", user_id: 1, category_id: 1}
+])
+User.create!([
+  {email: "robertdr85@gmail.com", user_name: "robdrosen", password_digest: "$2a$10$xiqba.GSaH4TXc0HQ1xpf.zYaAbVDrWuYLR2Jp.94KDeGh.dyJ3Si", first_name: "Robert", last_name: "Rosenberg", bio: "The creator of the commitment ledger application!", profile_picture: "imgur"}
 ])
