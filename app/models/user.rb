e4ee61ee921e51c61b241e48a9858544fb5d8d26
Robert Rosenberg <robertdr85@gmail.com>
2018-02-25 15:47:52 -0800

    Added sign up, login, log out and commitments index page

--- app/models/user.rb
+++ app/models/user.rb
@@ -2,4 +2,5 @@ class User < ApplicationRecord
   has_secure_password
   has_many :commitments
   validates :email, presence: true, uniqueness: true
+  validates :user_name, presence: true, uniqueness: true
 end
--- db/seeds.rb
+++ db/seeds.rb
@@ -2,10 +2,10 @@ Category.create!([
   {title: "Personal"},
   {title: "Work"}
 ])
+User.create!([
+  {email: "test@test.com", user_name: "tester1", password_digest: "$2a$10$dqZ8zii4eZZsFjFKNUwdlOzNnhVEPPbw/G5VuSLJmpkLS8Ux4AVYy", first_name: "Test", last_name: "Account", bio: "This account is for testing this apps functionality!", profile_picture: "https://pbs.twimg.com/profile_images/886372283106721792/zH5tTGlQ.jpg"}
+])
 Commitment.create!([
   {what: "Finish my capstone project", who: "myself", when: "2018-03-29 21:30:00", status: "Committed", notes: "I'm commiting myself to making this commitment app so that I fullfill my commitment on helping other commit to their commitments :)", user_id: 1, category_id: 2},
   {what: "Call my parents once a week", who: "Mom and Dad", when: nil, status: "Committed", notes: "Keep in better touch with my parents, to make them happy!", user_id: 1, category_id: 1}
-])
-User.create!([
-  {email: "robertdr85@gmail.com", user_name: "robdrosen", password_digest: "$2a$10$xiqba.GSaH4TXc0HQ1xpf.zYaAbVDrWuYLR2Jp.94KDeGh.dyJ3Si", first_name: "Robert", last_name: "Rosenberg", bio: "The creator of the commitment ledger application!", profile_picture: "imgur"}
 ])
