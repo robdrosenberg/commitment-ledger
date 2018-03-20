a905569d24476c9192d72d79eebed3b5fb50f4bd
Robert Rosenberg <robertdr85@gmail.com>
2018-03-19 21:03:54 -0700

    Added paperclip avatar

--- db/migrate/20180320035658_add_attachment_avatar_to_people.rb
+++ db/migrate/20180320035658_add_attachment_avatar_to_people.rb
@@ -1,4 +1,4 @@
-class AddAttachmentAvatarToPeople < ActiveRecord::Migration
+class AddAttachmentAvatarToPeople < ActiveRecord::Migration[5.1]
   def change
     add_attachment :people, :avatar
   end
